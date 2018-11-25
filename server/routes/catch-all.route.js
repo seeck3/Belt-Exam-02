82aa841667855d23debcb9e370adf48e19837187
Marco Seo <marco.dseo@gmail.com>
2018-11-25 15:38:26 -0500

    please

--- server/routes/catch-all.route.js
+++ server/routes/catch-all.route.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const path = require('path');
 
 router.all('*', function (_request, response) {
-  response.sendFile(path.resolve('dist/public/index.html'));
+  response.sendFile(path.resolve('dist/beltexam/index.html'));
 });
 
 module.exports = router;
